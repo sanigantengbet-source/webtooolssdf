84b3e9b0d0df8d104326570e7922e022779eb9d1
ALOK GANTENG <sanigantengbet@gmail.com>
2026-09-17 02:07:08 +0700

    Update navbar text from 'give me coffee' to 'buy me a coffee'

--- components/public/navbar.tsx
+++ components/public/navbar.tsx
@@ -77,7 +77,7 @@ export function Navbar({ onSearchClick }: NavbarProps) {
               className="inline-flex items-center gap-1.5 hover:text-[#171717] dark:hover:text-[#ededed] transition-colors"
             >
               <Coffee className="h-3.5 w-3.5" />
-              <span>give me coffe</span>
+              <span>buy me a coffee</span>
             </a>
           </nav>
         </div>
