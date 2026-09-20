ddc8762f397b6c6472b64211c118724dc48cfa28
ALOK GANTENG <sanigantengbet@gmail.com>
2026-09-20 12:28:26 +0700

    Upload next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**', // This allows any path under the hostname
       },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   output: 'standalone',
