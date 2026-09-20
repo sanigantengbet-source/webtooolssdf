398a864d730de9e421ae3c7b7400819d631073ca
ALOK GANTENG <sanigantengbet@gmail.com>
2026-09-20 12:29:42 +0700

    Upload app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,35 @@ export const metadata = {
   title: "SANN404 FORUM GROUP | Curated Tools Collection",
   description:
     "Curated collection of online web tools, utilities, automation scripts, and projects created and managed by SANN404 FORUM GROUP.",
+  openGraph: {
+    title: "SANN404 FORUM GROUP | Curated Tools Collection",
+    description:
+      "Curated collection of online web tools, utilities, automation scripts, and projects created and managed by SANN404 FORUM GROUP.",
+    type: "website",
+    siteName: "Tools Collection",
+    images: [
+      {
+        url: "https://i.ibb.co/v6sxPmTP/file-00000000348481fa9ff18e206e8219a9.png",
+        width: 1734,
+        height: 907,
+        alt: "Tools Collection - SANN404 FORUM GROUP",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "SANN404 FORUM GROUP | Curated Tools Collection",
+    description:
+      "Curated collection of online web tools, utilities, automation scripts, and projects created and managed by SANN404 FORUM GROUP.",
+    images: [
+      {
+        url: "https://i.ibb.co/v6sxPmTP/file-00000000348481fa9ff18e206e8219a9.png",
+        width: 1734,
+        height: 907,
+        alt: "Tools Collection - SANN404 FORUM GROUP",
+      },
+    ],
+  },
 };
 
 export default function LandingPage() {
