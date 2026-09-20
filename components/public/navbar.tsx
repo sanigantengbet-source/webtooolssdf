b682adb94c2deb77af30bab743c7eb560192b7d3
ALOK GANTENG <sanigantengbet@gmail.com>
2026-09-20 14:23:33 +0700

    Upload components/public/navbar.tsx

--- components/public/navbar.tsx
+++ components/public/navbar.tsx
@@ -202,7 +202,7 @@ export function Navbar({ onSearchClick }: NavbarProps) {
             className="flex items-center gap-2 py-2 text-sm text-[#171717] dark:text-[#ededed] hover:text-zinc-600 dark:hover:text-zinc-300"
           >
             <Coffee className="h-4 w-4" />
-            <span>buy me a coffee</span>
+            <span>give me coffe</span>
           </a>
           <div className="pt-2 border-t border-[#eaeaea] dark:border-[#27272a]">
             <Link
