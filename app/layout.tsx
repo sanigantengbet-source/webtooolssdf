7e43d3e6f579f197fc561fb3f632031e2ecc0e82
ALOK GANTENG <sanigantengbet@gmail.com>
2026-09-20 12:28:06 +0700

    Upload app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,17 +5,35 @@ import { ThemeProvider } from '@/components/ui/theme-provider';
 import { RouteProgress } from '@/components/ui/route-progress';
 
 export const metadata: Metadata = {
+  metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL || 'https://project.saannndec5ty.my.id'),
   title: 'Tools Collection - Curated Tools & Projects',
   description: 'A curated collection of web tools, websites and projects created and maintained by SANN404 FORUM GROUP',
   openGraph: {
     title: 'Tools Collection - Curated Tools & Projects',
     description: 'A curated collection of web tools, websites and projects created and maintained by SANN404 FORUM GROUP',
     type: 'website',
+    siteName: 'Tools Collection',
+    images: [
+      {
+        url: 'https://i.ibb.co/v6sxPmTP/file-00000000348481fa9ff18e206e8219a9.png',
+        width: 1734,
+        height: 907,
+        alt: 'Tools Collection - SANN404 FORUM GROUP',
+      },
+    ],
   },
   twitter: {
     card: 'summary_large_image',
     title: 'Tools Collection - Curated Tools & Projects',
     description: 'A curated collection of web tools, websites and projects created and maintained by SANN404 FORUM GROUP',
+    images: [
+      {
+        url: 'https://i.ibb.co/v6sxPmTP/file-00000000348481fa9ff18e206e8219a9.png',
+        width: 1734,
+        height: 907,
+        alt: 'Tools Collection - SANN404 FORUM GROUP',
+      },
+    ],
   },
 };
 
